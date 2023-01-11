58c7c08223c86e0fbd973dc0378e2860e7a0be64
hasanchamdany <hasanchamdany@gmail.com>
2023-01-11 21:16:50 +0700

    init react-native project

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello hasan wellcome back to react native project</Text>
+      {/* <Button>Hello</Button> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FAFAFAexpo ',
     alignItems: 'center',
     justifyContent: 'center',
   },
