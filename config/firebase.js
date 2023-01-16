eef53eba30feab2981d1ecca35778282ede6c21c
hasanchamdany <hasanchamdany@gmail.com>
2023-01-16 23:22:45 +0700

    fix(firebase): fix constant expo issue on android sdk

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import * as Expo from 'expo'
 import Constants from 'expo-constants';
 // Firebase config
 const firebaseConfig = {
@@ -11,13 +12,22 @@ const firebaseConfig = {
   // messagingSenderId: Constants.manifest.extra.messagingSenderId,
   // appId: Constants.manifest.extra.appId,
   // databaseURL: Constants.manifest.extra.databaseURL
-  apiKey: "AIzaSyBQDRYd3KU0yPN5-BnlVG9HD8xqjAyajUk",
-  authDomain: "chatappv1-8a7c1.firebaseapp.com",
-  projectId: "chatappv1-8a7c1",
-  storageBucket: "chatappv1-8a7c1.appspot.com",
-  messagingSenderId: "263246682787",
-  appId: "1:263246682787:web:b955f763863ac20b2afd16",
-  measurementId: "G-ZCX05F3R52"
+
+  // apiKey: "AIzaSyBQDRYd3KU0yPN5-BnlVG9HD8xqjAyajUk",
+  // authDomain: "chatappv1-8a7c1.firebaseapp.com",
+  // projectId: "chatappv1-8a7c1",
+  // storageBucket: "chatappv1-8a7c1.appspot.com",
+  // messagingSenderId: "263246682787",
+  // appId: "1:263246682787:web:b955f763863ac20b2afd16",
+  // measurementId: "G-ZCX05F3R52"
+
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.measurementId,
+  projectId: process.env.PROJECT_ID,
 };
 // initialize firebase
 initializeApp(firebaseConfig);
